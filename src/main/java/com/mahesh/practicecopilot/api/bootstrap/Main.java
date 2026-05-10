76602b6f72395653fffd0f0de450a456d4c8e31f
RELEVANTZ\maheshc <mahesh.chandrasekharraju@relevantz.com>
2026-05-10 11:29:56 +0530

    changes on URI on main

--- src/main/java/com/mahesh/practicecopilot/api/bootstrap/Main.java
+++ src/main/java/com/mahesh/practicecopilot/api/bootstrap/Main.java
@@ -7,18 +7,21 @@ import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import java.io.IOException;
 import java.net.URI;
 
+import jakarta.ws.rs.core.UriBuilder;
+
 public class Main {
-    public static final String BASE_URI = "http://0.0.0.0:8080/api/";
+    //public static final String BASE_URI = "http://0.0.0.0:8080/api/";
+    public static final URI baseUri = UriBuilder.fromUri("http://localhost/").port(8080).path("api/").build();
 
     public static HttpServer startServer() {
         final ApiApplicationConfig config = new ApiApplicationConfig();
-        return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), config);
+        return GrizzlyHttpServerFactory.createHttpServer(baseUri, config);
     }
 
     public static void main(String[] args) throws IOException {
         HttpServer server = startServer();
-        System.out.println("JAX-RS API started at " + BASE_URI);
-        System.out.println("Health endpoint: " + BASE_URI + "health");
+        System.out.println("JAX-RS API started at " + baseUri.toString());
+        System.out.println("Health endpoint: " + baseUri.toString() + "health");
         System.in.read();
         server.shutdownNow();
     }
